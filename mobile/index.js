06dc83977b17c4e9161826ea05f4255bc03b6c60
alialkh <32747088+alialkh@users.noreply.github.com>
2025-11-02 14:11:47 -0700

    Adjust prompt placeholder and polyfill PlatformConstraints

--- mobile/index.js
+++ mobile/index.js
@@ -1,4 +1,7 @@
 import { registerRootComponent } from 'expo';
+import { registerPlatformConstraintsPolyfill } from './src/polyfills/registerPlatformConstraintsPolyfill';
 import App from './App';
+
+registerPlatformConstraintsPolyfill();
 
 registerRootComponent(App);
